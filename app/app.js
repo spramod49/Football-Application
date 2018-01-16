17ed65d97130806771c6661f23aa6b10c3aaff43
Pramod <spramod49@gmail.com>
2018-01-17 00:11:20 +0530

    added styles.

--- app/app.js
+++ app/app.js
@@ -140,8 +140,6 @@ function teamResult($routeParams,yearFifteenData,yearSixteenData) {
     name:$routeParams.team,
     results: []
   };
-  var temp_win_diff =0;
-  var temp_loss_diff =0;
 
   for (let index = 0; index < this.yearFifteenData.rounds.length; index++) {
     for(let j = 0; j<this.yearFifteenData.rounds[index].matches.length; j++){
@@ -149,22 +147,17 @@ function teamResult($routeParams,yearFifteenData,yearSixteenData) {
       
       if(temp_obj.team1.name == $routeParams.team)
       {
-          //  if(this.yearFifteenData.rounds[index].matches[j].score1-this.yearFifteenData.rounds[index].matches[j].score2>temp_win_diff){
-          //     this.biggestWin.home.name = temp_obj.team2.name;
-          //     this.biggestWin.home.scoreline = this.yearFifteenData.rounds[index].matches[j].score1+"-"+this.yearFifteenData.rounds[index].matches[j].score2;
-          //  }
-          //  else if(this.yearFifteenData.rounds[index].matches[j].score1-this.yearFifteenData.rounds[index].matches[j].score2<temp_loss_diff){
-          //   this.biggestLoss.home.name = temp_obj.team2.name;
-          //   this.biggestLoss.home.scoreline = this.yearFifteenData.rounds[index].matches[j].score1+"-"+this.yearFifteenData.rounds[index].matches[j].score2;
-          //  }
           var result_decision = ()=>{
             if(this.yearFifteenData.rounds[index].matches[j].score1 > this.yearFifteenData.rounds[index].matches[j].score2){
+              this.games.won++;
               return "W";
             }
             else if (this.yearFifteenData.rounds[index].matches[j].score1 == this.yearFifteenData.rounds[index].matches[j].score2) {
+              this.games.draw++;
               return "D";
             }
             else{
+              this.games.lost++;
               return "L";
             }
           };
@@ -177,23 +170,17 @@ function teamResult($routeParams,yearFifteenData,yearSixteenData) {
           this.team.results.push(obj);
       }
       else if (temp_obj.team2.name === $routeParams.team) {
-
-      //   if(this.yearFifteenData.rounds[index].matches[j].score2-this.yearFifteenData.rounds[index].matches[j].score1>temp_win_diff){
-      //     this.biggestWin.away.name = temp_obj.team1.name;
-      //     this.biggestWin.away.scoreline = this.yearFifteenData.rounds[index].matches[j].score1+"-"+this.yearFifteenData.rounds[index].matches[j].score2;
-      //  }
-      //  else if(this.yearFifteenData.rounds[index].matches[j].score2-this.yearFifteenData.rounds[index].matches[j].score1<temp_loss_diff){
-      //   this.biggestLoss.away.name = temp_obj.team2.name;
-      //   this.biggestLoss.away.scoreline = this.yearFifteenData.rounds[index].matches[j].score1+"-"+this.yearFifteenData.rounds[index].matches[j].score2;
-      //  }
         var result_decision = ()=>{
           if(this.yearFifteenData.rounds[index].matches[j].score2 > this.yearFifteenData.rounds[index].matches[j].score1){
+            this.games.won++;
             return "W";
           }
           else if (this.yearFifteenData.rounds[index].matches[j].score1 == this.yearFifteenData.rounds[index].matches[j].score2) {
+            this.games.draw++;
             return "D";
           }
           else{
+            this.games.lost++;
             return "L";
           }
         };
@@ -207,21 +194,21 @@ function teamResult($routeParams,yearFifteenData,yearSixteenData) {
       }
     }
   }
-  for (let index = 0; index < this.team.results.length; index++) {
-    if (this.team.results[index].result == "W") {
-      this.games.won++;
-    } else if(this.team.results[index].result == "L"){
-      this.games.lost++;
-    }
-    else{
-      this.games.draw++;
-    }
-  }
+  // for (let index = 0; index < this.team.results.length; index++) {
+  //   if (this.team.results[index].result == "W") {
+  //     this.games.won++;
+  //   } else if(this.team.results[index].result == "L"){
+  //     this.games.lost++;
+  //   }
+  //   else{
+  //     this.games.draw++;
+  //   }
+  // }
   this.games.points = (this.games.won*3) + (this.games.draw*1);  
   this.showStatistics = false;
   this.showGames = false;
-
   console.log(this.team.results);
+  
 }
 
 app.controller("footballTable", footballTable);
